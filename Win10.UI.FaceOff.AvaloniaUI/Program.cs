23970c1949c66ccc0be3ffa11717afb2d57f64f3
The Sharp Ninja <ninja@thesharp.ninja>
2020-11-01 00:54:47 -0500

    Added more projects.

---
+++ Win10.UI.FaceOff.AvaloniaUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging.Serilog;
+
+using System;
+
+namespace Win10.UI.FaceOff.AvaloniaUI
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
---
+++ Win10.UI.FaceOff.Xamarin.Forms/Win10.UI.FaceOff.Xamarin.Forms/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Win10.UI.FaceOff.Xamarin.Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
